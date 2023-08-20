f13560725e53ba10323bb206556a4225416c1b27
jimjam <jamessparrow101@gmail.com>
2023-08-20 19:45:07 +0100

    plt attempt

--- sentdex/first.py
+++ sentdex/first.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import quandl
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
 import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL', api_key= "Ht3wGaVKYzgw6iAbhzRW")
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
@@ -19,7 +23,7 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forcast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forcast_out = int(math.ceil(0.01*len(df)))
+forcast_out = int(math.ceil(0.001*len(df)))
 
 df['label'] = df[forcast_col].shift(-forcast_out)
 
@@ -36,16 +40,42 @@ y = np.array(df['label'])
 X = preprocessing.scale(X)
 
 # remove all x where we dont have forcast
+X_lately = X[-forcast_out:]
 X = X[:-forcast_out]
+
+
 df.dropna(inplace=True)
 y = np.array(df['label'])
 
 print(len(X), len(y))
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1)
 
-clf = svm.SVR()
+clf = LinearRegression(n_jobs=10)
 clf.fit(X_train, y_train)
 accuaracy = clf.score(X_test, y_test)
 
-print(accuaracy)
+#print(accuaracy)
+
+forcast_set = clf.predict(X_lately)
+
+print(forcast_set, accuaracy, forcast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day_in_secs = 86400
+next_unix = last_unix + one_day_in_secs
+
+for i in forcast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day_in_secs
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df["Adj. Close"].plot()
+df["Forecast"].plot()
+plt.legend(loc=4)
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.show()
